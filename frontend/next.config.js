1e0be8a29ee8365c162c720b41a3320cce88dcf7
rat218578-cloud <rat218578@gmail.com>
2026-06-13 15:37:25 -0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',  // Mude de 'export' para 'standalone'
+  output: 'standalone',
   images: {
     unoptimized: true
   },
   swcMinify: true,
-  compress: true
+  compress: true,
+  staticPageGenerationTimeout: 120, // Aumenta timeout para 2 minutos
+  onDemandEntries: {
+    maxInactiveAge: 60 * 60 * 1000,
+    pagesBufferLength: 5
+  }
 }
 
 module.exports = nextConfig
