6e608a656032a6f42cfa0ba2d1a2735b60457d1f
rat218578-cloud <rat218578@gmail.com>
2026-06-13 14:45:55 -0300

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,11 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
   },
+  trailingSlash: true,
+  distDir: 'out'
 }
+
+module.exports = nextConfig
